f885e0218e55816344dfacd796c45e25593d3af7
dimoniche <dimoniche@gmail.com>
2016-05-11 20:38:38 +0300

    полностью установили шаблон 10

---
+++ StatMiBand10/StatMiBand/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using StatMiBand.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace StatMiBand.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
