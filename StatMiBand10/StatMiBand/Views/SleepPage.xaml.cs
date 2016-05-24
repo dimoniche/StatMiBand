62e7703442bfe715dcfbc0a5a85aa2f9fe5960b0
Дмитрий Чистяков <dimoniche@live.com>
2016-05-25 00:36:29 +0300

    вывел график

--- StatMiBand10/StatMiBand/Views/SleepPage.xaml.cs
+++ StatMiBand10/StatMiBand/Views/SleepPage.xaml.cs
@@ -9,6 +9,8 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Windows.Storage;
 using StatMiBand.Source;
+using System.Collections.Generic;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 namespace StatMiBand.Views
 {
@@ -24,8 +26,25 @@ namespace StatMiBand.Views
         private void button_Click(object sender, RoutedEventArgs e)
         {
             SourceData data = ((App)Application.Current).XmlData;
+
+            Random rand = new Random();
+            System.Collections.Generic.List<FinancialStuff> financialStuffList = new List<FinancialStuff>();
+            financialStuffList.Add(new FinancialStuff() { Name = "MSFT", Amount = rand.Next(0, 200) });
+            financialStuffList.Add(new FinancialStuff() { Name = "AAPL", Amount = rand.Next(0, 200) });
+            financialStuffList.Add(new FinancialStuff() { Name = "GOOG", Amount = rand.Next(0, 200) });
+            financialStuffList.Add(new FinancialStuff() { Name = "BBRY", Amount = rand.Next(0, 200) });
+            (PieChart.Series[0] as PieSeries).ItemsSource = financialStuffList;
+            //(ColumnChart.Series[0] as ColumnSeries).ItemsSource = financialStuffList;
+            //(LineChart.Series[0] as LineSeries).ItemsSource = financialStuffList;
         }
 
-
+  
     }
+
+    public class FinancialStuff
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+
 }
